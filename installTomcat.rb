a85fc03fee5790b3995e6fdb647440ef1bcd6f4d
Dan Rabinovitz <dsr1122@gmail.com>
2017-03-27 16:21:26 -0400

    work in process

--- installTomcat.rb
+++ installTomcat.rb
@@ -1,15 +1,13 @@
 #package 'java-1.7.0-openjdk-devel'
 
-#group 'chef' do
-#   action :create
-#end
+group 'chef' do
+   action :create
+end
 
-#user 'chef' do
-#   group 'chef'
-#   home '/home/chef'
-#   shell '/bin/bash'
-#   password 'test'
-#end
+user 'chef' do
+   group 'chef'
+   home '/home/chef'
+end
 
 execute 'get tomcast' do
   cwd '/tmp'
@@ -27,4 +25,15 @@ execute 'untar tomcat to directory' do
    command 'tar xvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1'
 end
 
+execute 'change group' do
+   cwd '/opt'
+   command 'chgrp -R chef tomcat'
+end
+
+execute 'change ownership' do
+   cwd '/opt/tomcat'
+   command 'chmod -R g+r conf'
+   command 'chmod g+x conf'
+   command 'chown -R chef webapps/ work/ temp/ logs/'
+end
 
