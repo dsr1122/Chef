fe333ab80f1cc15f9c74c5dbef9ccbcc12d63eae
Dan Rabinovitz <dsr1122@gmail.com>
2017-03-27 15:32:39 -0400

    In progress

--- installTomcat.rb
+++ installTomcat.rb
@@ -1,12 +1,30 @@
-package 'java-1.7.0-openjdk-devel'
+#package 'java-1.7.0-openjdk-devel'
 
-group 'chef' do
-   action :create
+#group 'chef' do
+#   action :create
+#end
+
+#user 'chef' do
+#   group 'chef'
+#   home '/home/chef'
+#   shell '/bin/bash'
+#   password 'test'
+#end
+
+execute 'get tomcast' do
+  cwd '/tmp'
+  creates 'apache-tomcat-8.5.12.tar.gz'
+  command 'wget http://supergsego.com/apache/tomcat/tomcat-8/v8.5.12/bin/apache-tomcat-8.5.12.tar.gz'
 end
 
-user 'chef' do
-   group 'chef'
-   home '/home/chef'
-   shell '/bin/bash'
-   password 'test'
+directory '/opt/tomcat' do
+   owner 'root'
+   group 'root'
 end
+
+execute 'untar tomcat to directory' do
+   cwd '/tmp'
+   command 'tar xvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1'
+end
+
+
