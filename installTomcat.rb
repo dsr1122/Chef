6de732a14084d3e7c1812617efb6a0b21776ed24
Dan Rabinovitz <dsr1122@gmail.com>
2017-03-28 14:37:57 -0400

    not quite working

--- installTomcat.rb
+++ installTomcat.rb
@@ -1,4 +1,4 @@
-#package 'java-1.7.0-openjdk-devel'
+package 'java-1.7.0-openjdk-devel'
 
 group 'chef' do
    action :create
@@ -6,7 +6,8 @@ end
 
 user 'chef' do
    group 'chef'
-   home '/home/chef'
+   home '/opt/tomcat'
+   shell '/bin/nologin'
 end
 
 execute 'get tomcast' do
@@ -15,13 +16,14 @@ execute 'get tomcast' do
   command 'wget http://supergsego.com/apache/tomcat/tomcat-8/v8.5.12/bin/apache-tomcat-8.5.12.tar.gz'
 end
 
-directory '/opt/tomcat' do
-   owner 'root'
-   group 'root'
+execute 'mkdir tomcat' do
+   cwd '/opt'
+   command 'mkdir tomcat'
 end
 
 execute 'untar tomcat to directory' do
    cwd '/tmp'
+   command 'mkdir /opt/tomcat'
    command 'tar xvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1'
 end
 
@@ -35,5 +37,34 @@ execute 'change ownership' do
    command 'chmod -R g+r conf'
    command 'chmod g+x conf'
    command 'chown -R chef webapps/ work/ temp/ logs/'
+end
+
+file '/etc/systemd/system/tomcat.service' do
+   content "# Systemd unit file for tomcat
+[Unit]
+Description=Apache Tomcat Web Application Container
+After=syslog.target network.target
+
+[Service]
+Type=forking
+
+Environment=JAVA_HOME=/usr/lib/jvm/jre
+Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
+Environment=CATALINA_HOME=/opt/tomcat
+Environment=CATALINA_BASE=/opt/tomcat
+Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
+Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
+
+ExecStart=/opt/tomcat/bin/startup.sh
+ExecStop=/bin/kill -15 $MAINPID
+
+User=chef
+Group=chef
+UMask=0007
+RestartSec=10
+Restart=always
+
+[Install]
+WantedBy=multi-user.target"
 end
 
